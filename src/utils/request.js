acd558f2338b4494cc688b937c5896d749379327
xwy1998 <11490343+xwy1998@user.noreply.gitee.com>
2022-09-24 10:49:24 +0800

    feat:端口调试

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 // 通过axios创建axios实例
 const service = axios.create({
-  baseURL: '/dev', // 基准地址
+  baseURL: process.env.VUE_APP_BASE_API, // 基准地址
   timeout: 5000 // request timeout
 })
 
--- vue.config.js
+++ vue.config.js
@@ -13,7 +13,7 @@ const name = defaultSettings.title || 'vue Admin Template' // page title
 // For example, Mac: sudo npm run
 // You can change the port by the following methods:
 // port = 9528 npm run dev OR npm run dev --port = 9528
-const port = process.env.port || process.env.npm_config_port || 9528 // dev port
+const port = process.env.port || process.env.npm_config_port || 9528
 
 // All configuration item explanations can be find in https://cli.vuejs.org/config/
 module.exports = {
@@ -27,8 +27,8 @@ module.exports = {
   publicPath: '/',
   outputDir: 'dist',
   assetsDir: 'static',
-  lintOnSave: process.env.NODE_ENV === 'development',
-  productionSourceMap: false,
+  lintOnSave: process.env.NODE_ENV === 'development', // 关闭语法检查
+  productionSourceMap: false, // 打包优化
   devServer: {
     port: port,
     open: true,
